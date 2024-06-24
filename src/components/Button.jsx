ca34a05cd23be52f2a970039b1dd628bb778766c
armaanjaj <armaan.jaj@gmail.com>
2024-06-23 19:16:06 -0600

    Setup initial project files

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import ButtonSvg from "../assets/svg/ButtonSvg";
+
+const Button = ({
+  className = "",
+  href = "",
+  onClick = "",
+  children,
+  px = "",
+  white = "",
+}) => {
+  const renderButton = () => (
+    <button>
+      <span>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
+  return <div></div>;
+};
+
+export default Button;
